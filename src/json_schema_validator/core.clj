1b19c56a939806bba0947bf276703fc4cc18bb98
Davey Wood <daveyw1234@gmail.com>
2014-07-08 23:14:20 -0400

    basic property validations, but the code is already a mess

--- src/json_schema_validator/core.clj
+++ src/json_schema_validator/core.clj
@@ -18,20 +18,41 @@
                       "number" validate-is-number
                       "integer" validate-is-integer})
 
+;each property is a node
+;validating a node with property means validating it, then validating its properties
+
+(declare get-property-validators)
+
+(defn applier [value]
+  "applies the validation function"
+  #(% value (str value)));TODO: real error message
+
+(defn property-applier [value]
+  #(% value))
 
 (defn build-validator-list-for-node [node]
   "builds a list of validators for a single node"
   (list (get type-validators (:type node))));TODO: validate properties, required, etc
-
-(defn applier [value]
-  "applies the validation function"
-  #(% value "fail"));TODO: real error message
+  ;(concat (list (get type-validators (:type node))) (get-property-validators node)));TODO: validate properties, required, etc
 
 (defn build-validator-for-node [node]
   "builds a validator for a single node"
-  #(filter (complement nil?) (map (applier %) (build-validator-list-for-node node) )))
+  #(filter (complement nil?) (concat (map (applier %) (build-validator-list-for-node node) ) (flatten (map (property-applier %) (get-property-validators node))))))
 
+(defn build-validator-for-property [node property]
+  "takes a node and a keyword (property) and builds a validator for that property"
+  (let [node-validator (build-validator-for-node (get (:properties node) property))]
+    #(node-validator (get % property))))
+
+(defn get-property-validators [node]
+  "get all the property validators for a node as a sequence"
+  (if (:properties node)
+    (map #(build-validator-for-property node %) (keys (:properties node)))
+    '()))
 
 (defn build-validator-from-schema [schema]
   "takes a json schema as a string and returns a function to validate maps based on that schema"
   (build-validator-for-node (json/read-str schema :key-fn keyword)))
+
+
+;(get-property-validators node)
--- test/json_schema_validator/core_test.clj
+++ test/json_schema_validator/core_test.clj
@@ -1,12 +1,20 @@
 (ns json-schema-validator.core-test
   (:require [clojure.test :refer :all]
+            [clojure.data.json :as json]
             [json-schema-validator.core :refer :all]))
 
 (def schema "{
     \"$schema\": \"http://json-schema.org/draft-04/schema#\",
     \"title\": \"Product\",
     \"description\": \"A product from Acme's catalog\",
-    \"type\": \"object\"
+    \"type\": \"object\",
+    \"properties\": {
+      \"id\": {
+        \"description\": \"The unique identifier for a product\",
+        \"type\": \"integer\"
+      }
+    },
+    \"required\": [\"id\"]
 }")
 
 
@@ -38,10 +46,53 @@
 
 (def validator (build-validator-from-schema schema)) ;builds a very basic validator
 
+(def property-validators (get-property-validators (json/read-str schema :key-fn keyword)))
+
+
+(deftest property-validator-length
+  (testing "testing property validator length"
+    (is (= 1 (count property-validators)))))
+
+(def id-property-validator (first property-validators))
+
+(deftest test-id-property-validator
+  (testing "test id property validator"
+    (is (empty? (id-property-validator {:id 8})))))
+
+
+;(def id-validator (build-validator-for-property {:properties { :id {:type "integer"}}} :id))
+(def id-validator (first (get-property-validators {:properties { :id {:type "integer"}}})))
+
+(deftest test-id-validator
+  (testing "testing that the id validator validates"
+    (is (empty? (id-validator {:id 8})))))
+
+(deftest test-id-validator-fail
+  (testing "testing that the id validator validates"
+    (is ((complement empty?) (id-validator "donkey")))))
+
+(def valid-product {:id 9})
+
+(def no-id-product {:blah "blah"})
+
+(def string-id-product {:id "blah"})
+
 (deftest test-validator
   (testing "testing the validator's true case"
-    (is (empty? (validator {:id 9})))))
+    (is (empty? (validator valid-product)))))
 
-(deftest test-validator-false
-  (testing "testing the validator's false case"
+(deftest test-validator-not-an-object
+  (testing "testing the validator with a value that is not a map"
     (is ((complement empty?) (validator 9)))))
+
+(deftest test-validator-invalid-object
+  (testing "testing the validator with an invalid map"
+    (is ((complement empty?) (validator no-id-product)))))
+
+(deftest test-validator-non-numeric-id
+  (testing "testing the validator with property of the wrong type"
+    (is ((complement empty?) (validator string-id-product)))))
+
+
+
+
